f5cc7c03e21cab655b72c56e82074107c72e68a0
aaron levin <vilevin@gmail.com>
2015-04-04 00:23:14 -0400

    Clean up processing

--- process.sh
+++ process.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
+
+readonly PROGNAME=$(basename $0)
+readonly ARGS="$@"
+
 IFS=''
 
+# exit program if unset
+exit_if_unset() {
+
+  local var="${1}"
+  local msg="${2}"
+
+  if [[ -z "${1}" ]]; then
+    echo "${2}"
+    exit -1
+  fi
+
+}
+
+# How to test for a list
 is_list() {
   echo "${1}" | grep -q "^   \*  "
 }
@@ -9,6 +27,8 @@ is_chapter() {
   echo "${1}" | grep -q "^[0-9]\."
 }
 
+# xml2rfc inserts a space between bullets. this
+# removes them.
 remove_extra_bullet_line() {
   local prev="${1}"
   while read cur; do
@@ -21,6 +41,32 @@ remove_extra_bullet_line() {
   done
 }
 
-cat "${1}" \
-  | sed 's/^   o  /   *  /g' \
-  | remove_extra_bullet_line 
+# Generate the resume using `xml2rfc`
+gen_resume_io() {
+
+  local file="${1}"
+  local out_file="${2}"
+
+  xml2rfc "${file}" \
+    -o "${out_file}" \
+    --text
+}
+
+main() {
+
+  exit_if_unset "${1}" "Must specify input file"
+  exit_if_unset "${2}" "Must specify output file"
+
+  local tmp_file=$(echo $RANDOM)
+
+  gen_resume_io "${1}" "${tmp_file}"
+
+  cat "${tmp_file}" \
+    | sed 's/^   o  /   *  /g' \
+    > "${2}"
+
+  rm "${tmp_file}"
+
+}
+
+main $@
