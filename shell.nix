f6d938edeea8aee481143cdeb4321bbaa8980565
aaron levin <vilevin@gmail.com>
2015-04-06 08:59:01 -0400

    Add rfc documents for accurate parsing

--- shell.nix
+++ shell.nix
@@ -28,8 +28,8 @@ nixpkgs.myEnvFun {
     nixpkgs.pythonPackages.lxml
     python.xml2rfc
     python.requests
-    nixpkgs.haskellngPackages.ghc-mod
-    nixpkgs.haskellngPackages.ghc
-    nixpkgs.haskellngPackages.hxt
+    nixpkgs.haskellPackages.ghcMod
+    nixpkgs.haskellPackages.ghc
+    nixpkgs.haskellPackages.hxt
   ];
 }
