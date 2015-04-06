e8c2f68b8609e9f56017f7fd866ad5ed6b6f9f7a
aaron levin <vilevin@gmail.com>
2015-04-05 20:46:15 -0400

    Add script to publish to github.io page

---
+++ publish.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# grab the name of the current branch
+current_branch() {
+  git rev-parse --abbrev-ref HEAD
+}
+
+main() {
+
+  local branch=$(current_branch)
+
+  git stash
+  git checkout gh-pages
+  cp resume.html index.html
+  git add index.html
+  git commit -m "publish resume"
+  git push origin gh-pages
+  git checkout "${branch}"
+  git stash pop
+}
+
+main $@
