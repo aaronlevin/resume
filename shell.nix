4ab8b4a643e11c05a9bb3fb2ae475523044e2966
aaronlevin <vilevin@gmail.com>
2021-08-12 23:05:13 -0400

    Update resume

--- shell.nix
+++ shell.nix
@@ -1,20 +1,20 @@
-{ nixpkgs ? (import <nixpkgs> {}) }:
 let
-  python = nixpkgs.pythonPackages // {
-    # package overrides here
-  };
-
-in
-nixpkgs.myEnvFun {
-  name = "resume";
+  pkgs = import <nixpkgs> {};
+  #nanomsg-py = ...build expression for this python library...;
+in pkgs.mkShell {
   buildInputs = [
-    # nixpkgs.vim
-    nixpkgs.python27
-    python.lxml
-    python.xml2rfc
-    python.requests
-    python.six
-    nixpkgs.haskellPackages.ghc
-    nixpkgs.haskellPackages.hxt
+    pkgs.python3
+    pkgs.python3.pkgs.lxml
+    pkgs.python3.pkgs.xml2rfc
+    pkgs.python3.pkgs.requests
+    pkgs.python3.pkgs.six
   ];
+  shellHook = ''
+    # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+    # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+    export PIP_PREFIX=$(pwd)/_build/pip_packages
+    export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+    export PATH="$PIP_PREFIX/bin:$PATH"
+    unset SOURCE_DATE_EPOCH
+  '';
 }
